f56e9549cc2f17de107d10b589dfbebad8394186
lxg776 <262621979@qq.com>
2017-12-22 17:11:19 +0800

    添加FMessage

---
+++ zheng/zheng-friend/zheng-friend-dao/src/main/java/com/zheng/friend/dao/model/FMessage.java
@@ -0,0 +1,147 @@
+package com.zheng.friend.dao.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class FMessage implements Serializable {
+    /**
+     * 编号
+     *
+     * @mbg.generated
+     */
+    private Integer id;
+
+    /**
+     * 发送者ID
+     *
+     * @mbg.generated
+     */
+    private Integer fromUserId;
+
+    /**
+     * 接受者ID
+     *
+     * @mbg.generated
+     */
+    private Integer toUserId;
+
+    /**
+     * 消息内容
+     *
+     * @mbg.generated
+     */
+    private String msgContent;
+
+    /**
+     * 消息状况(0:未读,1:已读)
+     *
+     * @mbg.generated
+     */
+    private Byte msgState;
+
+    /**
+     * 创建时间
+     *
+     * @mbg.generated
+     */
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Integer fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public Integer getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(Integer toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    public void setMsgContent(String msgContent) {
+        this.msgContent = msgContent;
+    }
+
+    public Byte getMsgState() {
+        return msgState;
+    }
+
+    public void setMsgState(Byte msgState) {
+        this.msgState = msgState;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", fromUserId=").append(fromUserId);
+        sb.append(", toUserId=").append(toUserId);
+        sb.append(", msgContent=").append(msgContent);
+        sb.append(", msgState=").append(msgState);
+        sb.append(", createTime=").append(createTime);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        FMessage other = (FMessage) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getFromUserId() == null ? other.getFromUserId() == null : this.getFromUserId().equals(other.getFromUserId()))
+            && (this.getToUserId() == null ? other.getToUserId() == null : this.getToUserId().equals(other.getToUserId()))
+            && (this.getMsgContent() == null ? other.getMsgContent() == null : this.getMsgContent().equals(other.getMsgContent()))
+            && (this.getMsgState() == null ? other.getMsgState() == null : this.getMsgState().equals(other.getMsgState()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getFromUserId() == null) ? 0 : getFromUserId().hashCode());
+        result = prime * result + ((getToUserId() == null) ? 0 : getToUserId().hashCode());
+        result = prime * result + ((getMsgContent() == null) ? 0 : getMsgContent().hashCode());
+        result = prime * result + ((getMsgState() == null) ? 0 : getMsgState().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        return result;
+    }
+}
